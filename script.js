132947ac59a5277374bfe44fa5c459f15e9e3c4e
keyon <keyongrayer@gmail.com>
2018-05-15 21:27:05 -0400

    Finished Lab

--- script.js
+++ script.js
@@ -1,87 +1,134 @@
 class Computer {
-    constructor (name,health) {
-      this.name=name;
-      this.health=health;
-    }
-    generateAttackDamage (max) {
-      return Math.floor(Math.random()*max)+1;
-    }
+  constructor(name, health) {
+    this.name = name;
+    this.health = health;
+  }
+  generateAttackDamage(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
 }
 
 class Character extends Computer {
- constructor(name,health,healsRemaining,wins) {
-  super(name,health);
-  this.healsRemaining=healsRemaining;
-  this.wins=wins;
+  constructor(name, health, healsRemaining, wins) {
+    super(name, health);
+    this.healsRemaining = healsRemaining;
+    this.wins = wins;
   }
-  heal () {
-    this.health=this.health+(Math.floor(Math.random()*10)+1);
+  heal() {
+    this.health = this.health + (Math.floor(Math.random() * 10) + 1);
     this.healsRemaining--;
   }
 }
-var classyUser=new Character("user",40,2,0);
-var classyGrant=new Computer('Grant',10);
+var classyUser = new Character("user", 40, 2, 0);
+var classyGrant = new Computer('Grant', 10);
 
-var startButton=document.getElementById("startButton");
-startButton.onclick=function () {
-  document.getElementById("game-wrapper").style.display="block";
-  document.getElementById("start-wrapper").style.display="none";
-  startGame ();
+var startButton = document.getElementById("startButton");
+startButton.onclick = function() {
+  document.getElementById("game-wrapper").style.display = "block";
+  document.getElementById("start-wrapper").style.display = "none";
+  startGame();
 }
-var playerName=document.getElementById("playerName");
+var playerName = document.getElementById("playerName");
+
 function player(name) {
   playerName.innerText = name;
 }
-var attackButton=document.getElementById("attackButton");
-attackButton.onclick = attack;
-function attack () {
-    classyUser.health-=classyUser.generateAttackDamage(5);
-    classyGrant.health-=classyGrant.generateAttackDamage(3);
-    var status=document.getElementById("updateArea");
-    function updateArea(theUpdate) {
-      status.innerText=theUpdate;
-    }
 
-    var playerHealthBar=document.getElementsByClassName("player")[0];
-    playerHealthBar.value=classyUser.health;
-    var grantHealthBar=document.getElementsByClassName("computer")[0];
-    grantHealthBar.value=classyGrant.health;
-    theUpdate=(`The user has ${classyUser.health} health left. Grant has ${classyGrant.health} health left.`);
-    updateArea(theUpdate);
-    if (classyGrant.health<=0) {
-          classyUser.wins++;
-          classyGrant.health=10;
-          console.log(`user wins are ${classyUser.wins}`);
-          var playerWinBar=document.getElementsByClassName("player")[2];
-          playerWinBar.value=classyUser.wins;
-          var grantHealthBar=document.getElementsByClassName("computer")[0];
-          grantHealthBar.value=classyGrant.health;
-        }
+var attackButton = document.getElementById("attackButton");
+attackButton.onclick = attack;
+
+function attack() {
+  var guessWho = playerName.innerText;
+  classyUser.health -= classyUser.generateAttackDamage(5);
+  classyGrant.health -= classyGrant.generateAttackDamage(3);
+  var status = document.getElementById("updateArea");
+
+function updateArea(theUpdate) {
+    status.innerText = theUpdate;
   }
-var quitButton=document.getElementById("quitButton");
-quitButton.onclick=quit;
-function quit () {
-  console.log(`user decided to quit.`);
+  var playerHealthBar = document.getElementsByClassName("player")[0];
+  playerHealthBar.value = classyUser.health;
+  var grantHealthBar = document.getElementsByClassName("computer")[0];
+  grantHealthBar.value = classyGrant.health;
+  theUpdate = (`${guessWho} has ${classyUser.health} health left. Grant has ${classyGrant.health} health left.`);
+  updateArea(theUpdate);
+  if (classyGrant.health <= 0) {
+    classyUser.wins++;
+
+    theUpdate=(`${guessWho} has ${classyUser.health} health left. Grant has ${classyGrant.health} health left. ${guessWho}'s wins are ${classyUser.wins}`);
+    updateArea(theUpdate);
+    var playerWinBar = document.getElementsByClassName("player")[2];
+    playerWinBar.value = classyUser.wins;
+    classyGrant.health = 10;
+    var grantHealthBar = document.getElementsByClassName("computer")[0];
+    grantHealthBar.value = classyGrant.health;
+  }
+  if (classyUser.wins>4) {
+    var status = document.getElementById("updateArea");
+    function updateArea(theUpdate) {
+        status.innerText = theUpdate;
+      }
+    theUpdate=(`The winner is ${guessWho}.`);
+    updateArea(theUpdate);
+    document.getElementById("healButton").style.display = "none";
+    document.getElementById("attackButton").style.display = "none";
+    document.getElementById("quitButton").style.display = "none";
+    }
+    if (classyUser.health<1){
+    var status = document.getElementById("updateArea");
+    function updateArea(theUpdate) {
+          status.innerText = theUpdate;
+        }
+    theUpdate=(`The winner is Grant.`);
+    updateArea(theUpdate)
+    document.getElementById("healButton").style.display = "none";
+    document.getElementById("attackButton").style.display = "none";
+    document.getElementById("quitButton").style.display = "none";
+    }
+}
+var quitButton = document.getElementById("quitButton");
+quitButton.onclick = quit;
+
+function quit() {
+  var guessWho = playerName.innerText;
+  var status = document.getElementById("updateArea");
+  function updateArea(theUpdate) {
+      status.innerText = theUpdate;
+    }
+  theUpdate=(`${guessWho} decided to quit.`);
+  updateArea(theUpdate);
+  document.getElementById("healButton").style.display = "none";
+  document.getElementById("attackButton").style.display = "none";
   return;
 }
-var healButton=document.getElementById("healButton")
-healButton.onclick=heal;
-function heal () {
+var healButton = document.getElementById("healButton");
+healButton.onclick = heal;
+
+
+function heal() {
+  var guessWho = playerName.innerText;
   classyUser.heal();
-   console.log(`user has healed`);
-   console.log(`user has ${classyUser.health} health left.`);
-   console.log(`Grant has ${classyGrant.health} health left`);
-   console.log(`You have ${classyUser.healsRemaining} heals left`);
-   var playerHealBar=document.getElementsByClassName("player")[1];
-   playerHealBar.value=classyUser.healsRemaining;
-   var playerHealthBar=document.getElementsByClassName("player")[0];
-   playerHealthBar.value=classyUser.health;
+  var status = document.getElementById("updateArea");
+  if (classyUser.healsRemaining<1) {
+    document.getElementById("healButton").style.display = "none";
+  }
+
+function updateArea(theUpdate) {
+    status.innerText = theUpdate;
+  }
+  theUpdate=(`${guessWho} has healed. ${guessWho} has ${classyUser.health} health left. Grant has ${classyGrant.health} health left. ${guessWho} has ${classyUser.healsRemaining} heals left.`);
+  updateArea(theUpdate);
+  var playerHealBar = document.getElementsByClassName("player")[1];
+  playerHealBar.value = classyUser.healsRemaining;
+  var playerHealthBar = document.getElementsByClassName("player")[0];
+  playerHealthBar.value = classyUser.health;
 }
 
-function startGame(){
-    var userName = prompt("What is your name?");
-    player (userName);
-  }
+function startGame() {
+  var userName = prompt("What is your name?");
+  player(userName);
+}
+
 
 // function startCombat (userName) {
 //   var classyUser=new Character(userName,40,2,0);
